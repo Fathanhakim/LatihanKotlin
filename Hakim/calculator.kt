e8be6d43e6b0c613e933accd0dc585bb58ef6fc1
Fathanhakim <92068101+Fathanhakim@users.noreply.github.com>
2021-10-08 13:34:14 +0700

    Hacktoberfest29

---
+++ Hakim/calculator.kt
@@ -0,0 +1,36 @@
+Calculator
+fun main(){
+     val sum: Int = 1 + 9
+     val sub: Int = 6 - 2
+     val multiplication: Int = 1 * 9
+     val division: Int = 56 / 7
+     val math: Int = 10 / (6 - 2) + 4 * 8
+     val math2: Int = 10 / 6 - 2 + 4 * 8
+
+
+     val a = sum.toDouble()
+     val b = sub.toDouble()
+     val c = multiplication.toDouble()
+     val d = division.toDouble()
+
+     val mod: Double = a % b
+     val mod2: Double = d % c
+     val mod3: Double = 10.0 % 3.3
+     val mod4: Double = 5.0 % 7
+
+   println(sum)
+   println(sub)
+   println(multiplication)
+   println(division)
+   println(division)
+   println(math)
+   println(math2)
+   println(a)
+   println(b)
+   println(c)
+   println(d)
+   println(mod)
+   println(mod2)
+      println(mod3)
+      println(mod4)
+}
